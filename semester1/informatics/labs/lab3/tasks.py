c63de342b53f3ba8e9a11d321a9eb515627747d2
Danil <danilperm8@gmail.com>
2024-10-26 14:18:52 +0300

    Informatic lab3

--- semester1/informatics/labs/lab3/tasks.py
+++ semester1/informatics/labs/lab3/tasks.py
@@ -6,28 +6,22 @@ def task1(s):
 
 def task2(s):
     print('Все слова, в которых 2 гласные стоят подряд, а после идёт слово, в котором не больше 3 согласных:')
-    s = re.split(r'\W|\d', s)
-    s = list(filter(None, s)) #Убираем пыстые строки
-    answ = []
-    for i in range(len(s) - 1):
-        flag_first_word = bool(re.fullmatch(r'\w*[аеёиоуыэюя]{2}\w*', s[i], flags=re.IGNORECASE))
-        flag_second_word = bool(re.fullmatch(r'(?:\w*[бвгджзйклмнпрстфхцчшщ]\w*){4,}', s[i + 1], flags=re.IGNORECASE))
-        if flag_first_word and not flag_second_word:
-            answ.append(s[i])
-
+    answ = re.finditer(r'\w*[аеёиоуыэюя]{2}\w*\b(?![\d\W]*(?:\w*[бвгджзйклмнпрстфхцчшщ]\w*){4,}\b)', s, flags=re.IGNORECASE)
+    flag = True
     for i in answ:
-        print(i)
-    if len(answ) == 0:
+        print(i[0])
+        flag = False
+    if flag:
         print("Ни одно слово не подходит")
     print()
 
 def task3(s):
     print('Слова, в которых встречается строго одна гласная буква:')
     s = re.finditer(r'\b[бвгджзйклмнпрстфхцчшщъь]*([аеёиоуюыэя])[бвгджзйклмнпрстфхцчшщъь]*(?:[бвгджзйклмнпрстфхцчшщъь]*\1[бвгджзйклмнпрстфхцчшщъь]*)*\b', s, flags=re.IGNORECASE)
+
     words = []
     for i in s:
         words.append([len(i[0]), i[0]])
-
     words.sort()
     for i in words:
         print(i[1])
