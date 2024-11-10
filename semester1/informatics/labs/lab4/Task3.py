14cd4c9d5876bdd67ee5cd922e0420b75b0adac8
Danil <danilperm8@gmail.com>
2024-11-11 00:16:37 +0300

    Lab4 informatics task3 well done

--- semester1/informatics/labs/lab4/Task3.py
+++ semester1/informatics/labs/lab4/Task3.py
@@ -1,7 +1,5 @@
 import re
 
-from Task1 import day_name
-
 timetableXML = open("src/Timetable.xml", mode="r", encoding="utf-8")
 timetableYAML = open("src/Timetable_task3.yaml", mode="w", encoding="utf-8")
 s = timetableXML.read()
@@ -11,10 +9,9 @@ s = re.sub(r'(?:</.*>)|(?:<\?.*\?>)|<', '', s)
 s = re.sub('>', ': ', s)
 s = re.sub('  lesson:', '- lesson:', s)
 
-days = re.findall(r'day name="\w*":', s)
+days = re.findall(r'(?<=day name=")\w*', s)
 for day in days:
-    day_name = re.search(r'(?<=day name=")\w*', day)
-    s = re.sub(f'  day name="{day_name[0]}":', f'- day:\n    \'@name\': {day_name[0]}', s)
+    s = re.sub(f'  day name="{day}":', f'- day:\n    \'@name\': {day}', s)
 
 #убираем пустые строки
 s = re.sub(r'^ *$\n', '', s, flags=re.MULTILINE)
