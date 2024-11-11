1eaa4fef8395b611b42ac81511f21bc123da0c17
Danil <danilperm8@gmail.com>
2024-11-12 01:02:56 +0300

    Lab4 informatics не работает

--- semester1/informatics/labs/lab4/Task3.py
+++ semester1/informatics/labs/lab4/Task3.py
@@ -1,8 +1,37 @@
 import re
 
+def XML_to_obj(XML_file):
+    s = XML_file.read()
+    start_tags_iters = re.finditer(r'<[^?/].+?>', s)
+    end_tags_iters = re.finditer(r'</[^?].+?>', s)
+    obj = []
+
+    #переведём из итераторов в многомерные массивы
+    start_tags = []
+    for i in start_tags_iters:
+        start_tags.append([i.group(), i.start(), i.end()])
+    end_tags = []
+    for i in end_tags_iters:
+        end_tags.append([i.group(), i.start(), i.end()])
+
+    for i in range(len(start_tags)):
+        j = len(start_tags) - 1 - i
+        #print(end_tags[i])
+        #new_obj = re.findall(fr'(?<=<{start_tags[i][2]}>).*?(?=</{end_tags[i][1]}>)', s, flags=re.MULTILINE)
+        new_obj = s[start_tags[i][2]:end_tags[j][1]]
+        obj.append(new_obj)
+        print(str(i) + ': ', new_obj)
+    print(obj)
+
+    return obj
+
+def obj_to_YAML(obj, YAML_file):
+    obj = '0'
+    s = obj
+    YAML_file.write(s)
+
 timetableXML = open("src/Timetable.xml", mode="r", encoding="utf-8")
-timetableYAML = open("src/Timetable_task3.yaml.yaml", mode="w", encoding="utf-8")
-s = timetableXML.read()
+timetableYAML = open("src/Timetable_task3.yaml", mode="w", encoding="utf-8")
 
-
-timetableYAML.write(s)
+obj = XML_to_obj(timetableXML)
+obj_to_YAML(obj, timetableYAML)
