7dd7a15374a2cbf3c63fae0c8bcc39824402e348
Danil <danilperm8@gmail.com>
2024-11-12 05:04:28 +0300

    Lab4 informatics парсинг XML без аттрибутов тегов

--- semester1/informatics/labs/lab4/Task3.py
+++ semester1/informatics/labs/lab4/Task3.py
@@ -1,5 +1,4 @@
 import re
-from inspect import stack
 
 def XML_to_obj(XML_file):
     s = XML_file.read()
@@ -10,28 +9,46 @@ def XML_to_obj(XML_file):
     for i in tags_iters:
         if re.fullmatch(r'<[^?/].+?>', i.group()):
             tags.append([i.group()[1:-1], "starting_tag", i.end()])
+            if re.search(r'".*?"', tags[-1][0]) is not None:
+                attributes = re.findall(r'\b\S+?=".*?"', tags[-1][0])
+                tags[-1][0] = re.search(r'.+?(?= \b\S*="\S+")', tags[-1][0]).group()
+                tags[-1].append(attributes)
         else:
             tags.append([i.group()[2:-1], "ending_tag", i.start()])
 
+    print(s)
     #Соберём данные в словарь
     stck = []
-    ma = {}
+    parent_map = {}
+    child_map = {}
     for i in range(len(tags)):
         if tags[i][1] == "starting_tag":
             stck.append(tags[i])
         else:
             text = s[stck[-1][2]:tags[i][2]]
-            #Если тег содержит дочерние теги, то изменим его
+            #Если элемент содержит дочерние элементы, то переложим его
             if re.search(r'(?:<[^/].+?>)|(?:</.+?>)', text) is not None:
-                text = re.findall(r'(?<=<)(.+?)(?=>)[\w\W]*?(?<=</)\1(?=>)', text)
+                text = set(re.findall(r'(?<=<)(.+?)(?: \b\S+?=\".*?\")*(?=>)[\w\W]*?(?<=</)\1(?=>)', text))
 
-            ma[stck[-1][0]] = text
-            print(stck[-1][0] + ': ', text)
+                # Создадим словарь со всеми дочерними объектами
+                for child_tag in text:
+                    child_map[child_tag] = parent_map[child_tag]
+                    parent_map.pop(child_tag)
+
+                if stck[-1][0] in parent_map.keys():
+                    parent_map[stck[-1][0]].append(child_map)
+                else:
+                    parent_map[stck[-1][0]] = [child_map]
+                child_map = {}
+            else:
+                parent_map[stck[-1][0]] = text
+
+            print(stck[-1][0] + ':', text)
             stck.pop()
     print()
-    print(ma)
+    print(parent_map)
 
-    return ma
+    return parent_map
 
 def obj_to_YAML(obj, YAML_file):
     obj = '0'
