4b6dca621232b0d193cf74a98ffe626971c84b7f
Danil <danilperm8@gmail.com>
2024-11-17 01:01:52 +0300

    opd lab1 dop done

--- semester1/OPD/lab1/dop/dop.sh
+++ semester1/OPD/lab1/dop/dop.sh
@@ -1,23 +1,21 @@
 echo "Самый часто запрашиваемый URL:"
-awk '{print $7}' file.txt | sort | uniq -c | sort -nr | head -n 1
+awk '{print $7}' access.log | sort | uniq -c | sort -nr | head -n 1
 
 > browsers.txt
 > os.txt
 > bots.txt
-awk -F '"' '{print $6}' file.txt | sort | uniq | grep -v '^\W*$' > user_agents.txt
+awk -F '"' '{print $6}' access.log | sort | uniq | grep -v '^\W*$' > user_agents.txt
 
 echo "Android:" >> os.txt
 grep Android user_agents.txt | grep -vE "[Bb]ot|rob" >> os.txt
-echo -e "\nBB10:" >> os.txt
-grep BB10 user_agents.txt | grep -vE "[Bb]ot|rob" >> os.txt
-echo -e "\niPad:" | grep -vE "like.*iPhone" >> os.txt
+echo -e "\niPad:" >> os.txt
 grep "CPU OS" user_agents.txt | grep -vE "[Bb]ot|rob" >> os.txt
-echo -e "\niPhone:" | grep -vE "like.*iPhone" >> os.txt
-grep "iPhone OS" user_agents.txt | grep -vE "[Bb]ot|rob" >> os.txt
+echo -e "\niPhone:" >> os.txt
+grep "CPU iPhone OS" user_agents.txt | grep -vE "[Bb]ot|rob" >> os.txt
+echo -e "\nLinux:" >> os.txt
+grep "Linux x" user_agents.txt | grep -vE "[Bb]ot|rob" >> os.txt
 echo -e "\nMac OS:" >> os.txt
 grep "Mac OS" user_agents.txt | grep -vE "like.*Mac OS" | grep -vE "[Bb]ot|rob" >> os.txt
-echo -e "\nUbuntu:" >> os.txt
-grep Ubuntu user_agents.txt | grep -vE "[Bb]ot|rob" >> os.txt
 echo -e "\nWindows:" >> os.txt
 grep Windows user_agents.txt | grep -vE "[Bb]ot|rob" >> os.txt
 
@@ -33,6 +31,6 @@ grep Firefox user_agents.txt >> browsers.txt
 echo -e "\nOpera:" >> browsers.txt
 grep Opera user_agents.txt >> browsers.txt
 echo -e "\nSafari:" >> browsers.txt
-grep -E "Version/.* Safari" user_agents.txt >> browsers.txt
+grep -E "Version/.* Safari" user_agents.txt | grep -v Android >> browsers.txt
 
-echo "Парсинг завершён"
+echo "Парсинг завершён. User-Agents в файлах browsers.txt, os.txt, bots.txt"
