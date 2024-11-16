e0c2d71fd3ff0d4e4c703aa078e3e4166007ed94
Danil <danilperm8@gmail.com>
2024-11-17 00:30:09 +0300

    opd lab 1 dop убираю ботов из общего вывода в ОС

--- semester1/OPD/lab1/dop/dop.sh
+++ semester1/OPD/lab1/dop/dop.sh
@@ -1,6 +1,38 @@
-echo "Самый часто запрашиваемый URL:" > result.txt
-awk '{print $7}' access.log | sort | uniq -c | sort -nr | head -n 1  >> result.txt
-echo -e "\nСписок User-Agent:" >> result.txt
-awk -F '"' '{print $6}' access.log | awk '{print $1}' | awk -F '/[0-9]' '{print $1}' | sort | uniq | grep -v '^\W*$' >> result.txt
+echo "Самый часто запрашиваемый URL:"
+awk '{print $7}' file.txt | sort | uniq -c | sort -nr | head -n 1
 
-echo "Парсинг завершён. Результаты сохранены в файл result.txt"
+> browsers.txt
+> os.txt
+> bots.txt
+awk -F '"' '{print $6}' file.txt | sort | uniq | grep -v '^\W*$' > user_agents.txt
+
+echo "Android:" >> os.txt
+grep Android user_agents.txt | grep -vE "[Bb]ot|rob" >> os.txt
+echo -e "\nBB10:" >> os.txt
+grep BB10 user_agents.txt | grep -vE "[Bb]ot|rob" >> os.txt
+echo -e "\niPad:" | grep -vE "like.*iPhone" >> os.txt
+grep "CPU OS" user_agents.txt | grep -vE "[Bb]ot|rob" >> os.txt
+echo -e "\niPhone:" | grep -vE "like.*iPhone" >> os.txt
+grep "iPhone OS" user_agents.txt | grep -vE "[Bb]ot|rob" >> os.txt
+echo -e "\nMac OS:" >> os.txt
+grep "Mac OS" user_agents.txt | grep -vE "like.*Mac OS" | grep -vE "[Bb]ot|rob" >> os.txt
+echo -e "\nUbuntu:" >> os.txt
+grep Ubuntu user_agents.txt | grep -vE "[Bb]ot|rob" >> os.txt
+echo -e "\nWindows:" >> os.txt
+grep Windows user_agents.txt | grep -vE "[Bb]ot|rob" >> os.txt
+
+echo "Bots:" >> bots.txt
+grep -E "[Bb]ot|rob" user_agents.txt >> bots.txt
+
+echo "Chrome:" >> browsers.txt
+grep Chrome user_agents.txt >> browsers.txt
+echo -e "\nEdge:" >> browsers.txt
+grep Edge user_agents.txt >> browsers.txt
+echo -e "\nFirefox:" >> browsers.txt
+grep Firefox user_agents.txt >> browsers.txt
+echo -e "\nOpera:" >> browsers.txt
+grep Opera user_agents.txt >> browsers.txt
+echo -e "\nSafari:" >> browsers.txt
+grep -E "Version/.* Safari" user_agents.txt >> browsers.txt
+
+echo "Парсинг завершён"
