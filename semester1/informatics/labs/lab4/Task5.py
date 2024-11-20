7bfdac3b8e4d59400a6764a65158cffa37386ec6
Danil <danilperm8@gmail.com>
2024-11-21 00:58:20 +0300

    Lab4 informatics task5 done

--- semester1/informatics/labs/lab4/Task5.py
+++ semester1/informatics/labs/lab4/Task5.py
@@ -1,33 +1,72 @@
-import xml.etree.ElementTree as ET
 import toml
+import re
 
-def xml_to_dict(element):
-    """Рекурсивно преобразует XML-элемент в словарь или список."""
-    children = list(element)
-    if not children:  # Если нет дочерних элементов, возвращаем текст
-        return element.text
-    grouped = {}
-    for child in children:
-        if child.tag not in grouped:
-            grouped[child.tag] = []
-        grouped[child.tag].append(xml_to_dict(child))
-    # Если тег содержит только один дочерний элемент, убираем список
-    for key in grouped:
-        if len(grouped[key]) == 1:
-            grouped[key] = grouped[key][0]
-    return grouped
+def XML_to_obj(XML_file):
+    s = XML_file
+    tags_iters = re.finditer(r'(?:<[^?/].+?>)|(?:</[^?].+?>)', s)
 
-def xml_to_toml(xml_string):
-    """Преобразует XML-строку в TOML-строку."""
-    root = ET.fromstring(xml_string)
-    xml_dict = {root.tag: xml_to_dict(root)}
-    return toml.dumps(xml_dict)
+    #переведём из итераторов в многомерные массивы
+    tags = []
+    for i in tags_iters:
+        if re.fullmatch(r'<[^?/].+?>', i.group()):
+            tags.append([i.group()[1:-1], "starting_tag", i.end(), []])
+            if re.search(r'".*?"', tags[-1][0]) is not None:
+                attributes = re.findall(r'\b\S+?=".*?"', tags[-1][0])
+                tags[-1][0] = re.search(r'.+?(?= \b\S*="\S+")', tags[-1][0]).group()
+                tags[-1][3] = attributes
+        else:
+            tags.append([i.group()[2:-1], "ending_tag", i.start(), []])
 
-timetableXML = open("src/Timetable.xml", mode="r", encoding="utf-8")
+    #Соберём данные в словарь
+    stck = []
+    parent_map = {}
+    child_map = {}
+    for i in range(len(tags)):
+        if tags[i][1] == "starting_tag":
+            stck.append(tags[i])
+        else:
+            text = s[stck[-1][2]:tags[i][2]]
+            #Проверим на наличие атрибутов и добавим их в качестве объекта
+            if len(stck[-1][3]) > 0:
+                attributes = ''
+                for i in stck[-1][3]:
+                    buf_tag = re.search(r'\b\w+(?==)', i).group()
+                    buf_inf = re.search(r'(?<=").*?(?=")', i).group()
+                    attributes += '<' + buf_tag + '>' + buf_inf + '</' + buf_tag + '>'
+
+            #Если элемент содержит дочерние элементы, то переложим его
+            if re.search(r'(?:<[^/].+?>)|(?:</.+?>)', text) is not None:
+                text = re.findall(r'(?<=<)(.+?)(?: \b\S+?=\".*?\")*(?=>)[\w\W]*?(?<=</)\1(?=>)', text)
+                text_set = set()
+                j = 0
+                while j < len(text):
+                    if text[j] in text_set:
+                        text.remove(text[j])
+                    else:
+                        text_set.add(text[j])
+                        j += 1
+
+                # Создадим словарь со всеми дочерними объектами
+                for child_tag in text:
+                    child_map[child_tag] = parent_map[child_tag]
+                    parent_map.pop(child_tag)
+
+                if stck[-1][0] in parent_map.keys():
+                    parent_map[stck[-1][0]].append(child_map)
+                else:
+                    parent_map[stck[-1][0]] = [child_map]
+                child_map = {}
+            else:
+                parent_map[stck[-1][0]] = text
+            stck.pop()
+    return parent_map
+
+timetableXML = open("src/Timetable.xml", mode="r", encoding="utf-8").read()
 timetableTOML = open("src/Timetable_task5.toml", mode="w", encoding="utf-8")
-s = timetableXML.read()
 
-# Преобразование XML в TOML
-toml_data = xml_to_toml(s)
+xml_dict = XML_to_obj(timetableXML)
+
+toml_data = toml.dumps(xml_dict)
+toml_data = re.sub(r']\n+\[', ']\n[', toml_data)
+
 timetableTOML.write(toml_data)
-print(toml_data)
