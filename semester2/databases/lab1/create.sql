564ee77f05407fa0717894a61da6753ce6b8915a
Danil <danilperm8@gmail.com>
2025-03-10 00:12:26 +0300

    databases lab1 fixed

--- semester2/databases/lab1/create.sql
+++ semester2/databases/lab1/create.sql
@@ -1,31 +1,31 @@
 CREATE TABLE weather (
-     id          INTEGER PRIMARY KEY,
+     id          SERIAL PRIMARY KEY,
      habitable    BOOLEAN NOT NULL,
      temperature INTEGER NOT NULL
 );
 CREATE TABLE location (
-      id         INTEGER PRIMARY KEY,
+      id         SERIAL PRIMARY KEY,
       name       VARCHAR(50) NOT NULL,
       id_weather INTEGER REFERENCES weather (id)
 );
 CREATE TABLE engine (
-    id      INTEGER PRIMARY KEY,
+    id      SERIAL PRIMARY KEY,
     power   INTEGER NOT NULL,
     working BOOLEAN NOT NULL
 );
 CREATE TABLE spaceship (
-       id          INTEGER PRIMARY KEY,
+       id          SERIAL PRIMARY KEY,
        name        VARCHAR(50) NOT NULL,
        location_id INTEGER REFERENCES location (id)
 );
 CREATE TABLE role
 (
-id   INTEGER PRIMARY KEY,
+id   SERIAL PRIMARY KEY,
 name VARCHAR(50) NOT NULL
 );
 CREATE TABLE human
 (
-id           INTEGER PRIMARY KEY,
+id           SERIAL PRIMARY KEY,
 name         VARCHAR(50) NOT NULL,
 birth_date   DATE        NOT NULL,
 role_id      INTEGER REFERENCES role (id),
@@ -33,12 +33,12 @@ spaceship_id INTEGER REFERENCES spaceship (id)
 );
 CREATE TABLE battery
 (
-id     INTEGER PRIMARY KEY,
+id     SERIAL PRIMARY KEY,
 charge REAL NOT NULL
 );
 CREATE TABLE antenna
 (
-id           INTEGER PRIMARY KEY,
+id           SERIAL PRIMARY KEY,
 battery_id   INTEGER REFERENCES battery (id),
 direction_id INTEGER REFERENCES location (id)
 );
--- semester2/databases/lab1/fill.sql
+++ semester2/databases/lab1/fill.sql
@@ -1,37 +1,37 @@
-INSERT INTO weather (id, habitable, temperature)
-VALUES (1, true, 25),
-       (2, false, -100),
-       (3, false, 8000),
-       (4, false, 20);
-INSERT INTO location (id, name, id_weather)
-VALUES (1, 'Mars', 4),
-       (2, 'Earth', 1),
-       (3, 'Neptune', 2),
-       (4, 'Sun', 3);
-INSERT INTO engine (id, power, working)
-VALUES (1, 5000, true),
-       (2, 3000, false),
-       (3, 4500, true);
-INSERT INTO spaceship (id, name, location_id)
-VALUES (1, 'Big Destroyer', 4),
-       (2, 'Mars Explorer', 1),
-       (3, 'Union', 2);
-INSERT INTO role (id, name)
-VALUES (1, 'Captain'),
-       (2, 'Engineer'),
-       (3, 'Scientist');
-INSERT INTO human (id, name, birth_date, role_id, spaceship_id)
-VALUES (1, 'Pul', '1990-05-15', 1, 2),
-       (2, 'Alice Smith', '1985-10-20', 2, 2),
-       (3, 'Bob Brown', '2000-01-30', 3, 2);
-INSERT INTO battery (id, charge)
-VALUES (1, 100.0),
-       (2, 0),
-       (3, 45.0);
-INSERT INTO antenna (id, battery_id, direction_id)
-VALUES (1, 2, 2),
-       (2, 3, 2),
-       (3, 1, 3);
+INSERT INTO weather (habitable, temperature)
+VALUES (true, 25),
+       (false, -100),
+       (false, 8000),
+       (false, 20);
+INSERT INTO location (name, id_weather)
+VALUES ('Mars', 4),
+       ('Earth', 1),
+       ('Neptune', 2),
+       ('Sun', 3);
+INSERT INTO engine (power, working)
+VALUES (5000, true),
+       (3000, false),
+       (4500, true);
+INSERT INTO spaceship (name, location_id)
+VALUES ('Big Destroyer', 4),
+       ('Mars Explorer', 1),
+       ('Union', 2);
+INSERT INTO role (name)
+VALUES ('Captain'),
+       ('Engineer'),
+       ('Scientist');
+INSERT INTO human (name, birth_date, role_id, spaceship_id)
+VALUES ('Pul', '1990-05-15', 1, 2),
+       ('Alice Smith', '1985-10-20', 2, 2),
+       ('Bob Brown', '2000-01-30', 3, 2);
+INSERT INTO battery (charge)
+VALUES (100.0),
+       (0),
+       (45.0);
+INSERT INTO antenna (battery_id, direction_id)
+VALUES (2, 2),
+       (3, 2),
+       (1, 3);
 INSERT INTO human_antenna (human_id, antenna_id)
 VALUES (1, 2),
        (2, 2),
