75d30738f2ac07fc1c68af49722940f9ebe05d96
Danil <danilperm8@gmail.com>
2025-06-15 15:52:35 +0300

    lab8 prog

--- semester2/programming/lab8/lab-server/src/main/java/com/lab8/server/managers/ExecutorInterface.java
+++
@@ -1,9 +0,0 @@
-package com.lab8.server.managers;
-
-import com.lab8.common.models.MusicBand;
-import com.lab8.common.utility.ExecutionStatus;
-import com.lab8.common.utility.Pair;
-
-public interface ExecutorInterface {
-    ExecutionStatus runCommand(String[] userCommand, MusicBand musicBand, Pair<String, String> user);
-}
