794303ab317713ec49f9d2929727e1f881b06357
Danil <danilperm8@gmail.com>
2025-10-31 22:54:35 +0300

    lab5 progLangs

---
+++ semester3/ProgLangs/lab5/Program.cs
@@ -0,0 +1,14 @@
+﻿var tasks = args.Select(async fileName =>
+{
+    try
+    {
+        var content = await File.ReadAllTextAsync(fileName);
+        var wordCount = content.Split([' ', '\n'], StringSplitOptions.RemoveEmptyEntries).Length;
+        Console.WriteLine($"File: {fileName}, Word Count: {wordCount}");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"File: {fileName}, Error: {ex.Message}");
+    }
+});
+await Task.WhenAll(tasks);
