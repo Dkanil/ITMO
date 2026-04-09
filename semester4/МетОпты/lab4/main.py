8ea24815866cb2966a5d84740f849e2cbab87e9e
Danil <danilperm8@gmail.com>
2026-04-09 23:40:36 +0300

    Метопы lab4

--- semester4/МетОпты/lab4/main.py
+++ semester4/МетОпты/lab4/main.py
@@ -1,13 +1,17 @@
 import numpy as np
 
+
 def func(x1, x2):
     return 2 * x1 ** 2 + 2 * x1 * x2 + 3 * x2 ** 2 - 10 * x1 - 10 * x2 + 15
+
 
 def df(f, x, eps):
     return (f(x + eps) - f(x)) / eps
 
+
 def ddf(f, x, eps):
     return (f(x + eps) - 2 * f(x) + f(x - eps)) / (eps * eps)
+
 
 def nuton_method(f, x, eps):
     while True:
@@ -16,6 +20,7 @@ def nuton_method(f, x, eps):
         if abs(df1) <= eps:
             return x
         x = x - df1 / ddf1
+
 
 def coordinate_spusk(x, eps):
     iters = 0
@@ -28,30 +33,39 @@ def coordinate_spusk(x, eps):
             return x, func(x[0], x[1]), iters
 
 
-def grad_f(x1, x2):
-    return np.array([4 * x1 + 2 * x2 - 10, 2 * x1 + 6 * x2 - 10])
+def grad_f(x1, x2, eps):
+    df1 = df(lambda x: func(x, x2), x1, eps)
+    df2 = df(lambda x: func(x1, x), x2, eps)
+    return np.array([df1, df2])
+
 
 def gradient_spusk(x, eps, alpha=0.1, find_min=True):
     iters = 0
     while True:
         prev = x.copy()
-        g = grad_f(x[0], x[1])
+        g = grad_f(x[0], x[1], eps)
         if find_min:
             g = -g
         x = x + alpha * g
         iters += 1
         if np.linalg.norm(g) <= eps or abs(func(prev[0], prev[1]) - func(x[0], x[1])) <= eps:
             return x, func(x[0], x[1]), iters
+        if ((find_min and func(x[0], x[1]) > func(prev[0], prev[1])) or
+                (not find_min and func(x[0], x[1]) < func(prev[0], prev[1]))):
+            x = prev
+            alpha *= 0.5
+
 
 def fastest_spusk(x, eps, find_min=True):
     iters = 0
     while True:
         prev = x.copy()
-        g = grad_f(x[0], x[1])
+        g = grad_f(x[0], x[1], eps)
         s_k = g / np.linalg.norm(g)
         if find_min:
             s_k = -s_k
-        phi = lambda h: func(x[0] + h * s_k[0], x[1] + h * s_k[1]) # подставляем в функцию следующие координаты, которые зависят от h
+        phi = lambda h: func(x[0] + h * s_k[0],
+                             x[1] + h * s_k[1])  # подставляем в функцию следующие координаты, которые зависят от h
         h = nuton_method(phi, 1, eps)
 
         x = x + h * s_k
@@ -66,6 +80,7 @@ def print_results(method_name, result):
     print(f"f(x, y): {result[1]:.5f}")
     print("Число итераций:", result[2], "\n")
 
+
 def main():
     x0 = np.array([-2.0, -2.0])
     eps = 0.0001
@@ -77,6 +92,7 @@ def main():
     print_results("Покоординатный спуск", coordinate_spusk(x0.copy(), eps))
     print_results("Градиентный спуск", gradient_spusk(x0.copy(), eps))
     print_results("Наискорейший спуск", fastest_spusk(x0.copy(), eps))
+
 
 if __name__ == "__main__":
     main()
